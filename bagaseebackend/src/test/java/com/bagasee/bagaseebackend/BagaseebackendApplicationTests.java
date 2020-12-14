fa220fb94cf5d3c87c87ae25a1c9ae794d0e5fbf
CindoddCindy <cindodcindy@gmail.com>
2020-12-14 12:57:33 +0700

    tambah polder

---
+++ bagaseebackend/src/test/java/com/bagasee/bagaseebackend/BagaseebackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bagasee.bagaseebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BagaseebackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
